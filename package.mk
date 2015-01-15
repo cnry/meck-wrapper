4600ba00386d66649140e8c0f1bac619ba6a4971
Lincoln de Sousa <lincoln@comum.org>
2015-01-15 18:48:19 -0500

    RabbitMQ wrapper for meck: https://github.com/eproxus/meck

---
+++ package.mk
@@ -0,0 +1,5 @@
+APP_NAME		:= meck
+UPSTREAM_GIT		:= https://github.com/eproxus/meck.git
+UPSTREAM_REVISION	:= 0.8.2
+ORIGINAL_APP_FILE	:= $(CLONE_DIR)/src/$(APP_NAME).app.src
+DO_NOT_GENERATE_APP_FILE:= true
